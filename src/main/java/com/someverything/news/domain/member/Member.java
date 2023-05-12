04db89986ce6245255b4cea40227982b5594630a
GMX\ZZ01WI766 <ksmhero@naver.com>
2023-05-12 13:51:31 +0900

    feat: Member 엔티티, MemberDetail 엔티티 작성 및 연관 관계 설정

--- src/main/java/com/someverything/news/domain/member/Member.java
+++ src/main/java/com/someverything/news/domain/member/Member.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Member")
@@ -14,11 +15,68 @@ import javax.persistence.*;
 @Builder
 @Data
 public class Member {
+    /** 회원 번호(PK) */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /** 닉네임 */
     private String nickname;
+
+    /** 토큰 */
     private String accessToken;
-    private String email;
-    
+
+    /** 토큰 갱신 일시 */
+    private LocalDateTime tokenRenewDt;
+
+    /** 가입 일시 */
+    private LocalDateTime joinDt;
+
+    /** 가입 경로 코드 */
+    private String joinPathCode;
+
+    /** 이메일 수신 여부 */
+    private String isEmailReceive;
+
+    /** 이메일 수신 동의/거부 일시 */
+    private LocalDateTime emailReceiveDt;
+
+    /** SMS 수신 여부 */
+    private String isSmsReceive;
+
+    /** SMS 수신 동의/거부 일시 */
+    private LocalDateTime smsReceiveDt;
+
+    /** 회원 상태 코드(01 : 정상, 02 : 비밀번호 오류 5회, 03 : 휴면, 04: 재가입, 05: ) */
+    private String statusCode;
+
+    /** 비밀번호 */
+    private String password;
+
+    /** 비밀번호 초기화 여부 */
+    private String isPwdInit;
+
+    /** 비밀번호 변경 일시 */
+    private LocalDateTime pwdChangeDt;
+
+    /** 로그인 실패 횟수 */
+    private Integer loginFailCnt;
+
+    /** 최종 로그인 일시 */
+    private LocalDateTime lastLoginDt;
+
+    /** 접근 디바이스 종류 */
+    private String deviceType;
+
+    /** 접근 IP */
+    private String loginIp;
+
+    /** 휴면 적용 일시 */
+    private LocalDateTime dormantApplyDt;
+
+    /** 약관 동의 항목 */
+    private String termsAgreement;
+
+    @OneToOne(mappedBy = "member")
+    private MemberDetail memberDetail;
 }
---
+++ src/main/java/com/someverything/news/domain/member/MemberDetail.java
@@ -0,0 +1,54 @@
+package com.someverything.news.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Member_Detail")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class MemberDetail {
+    /** PK */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /** 회원 번호(FK) */
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    /** 회원 명 */
+    private String memberNm;
+
+    /** 생년월일 */
+    private String birth;
+
+    /** 주민번호 */
+    private String residentNumber;
+
+    /** 전화 */
+    private String tel;
+
+    /** 휴대폰 */
+    private String mobileNumber;
+
+    /** 이메일 */
+    private String email;
+
+    /** 성별 구분 코드 */
+    private String genderCode;
+
+    /** 국적 구분 코드 */
+    private String nationCode;
+
+    /** 개인정보 수정 일시 */
+    private LocalDateTime infoUpdateDt;
+}
