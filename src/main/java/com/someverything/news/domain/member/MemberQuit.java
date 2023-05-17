1fe8d70f1cabb860470f6bd92aa603601ed75919
ksmhero <ksmhero@naver.com>
2023-05-17 23:16:18 +0900

    feat: MemberQuit.java 엔티티 작성

--- src/main/java/com/someverything/news/domain/member/MemberQuit.java
+++ src/main/java/com/someverything/news/domain/member/MemberQuit.java
@@ -1,7 +1,36 @@
 package com.someverything.news.domain.member;
 
 import com.someverything.news.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Member_Quit")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class MemberQuit extends BaseEntity {
+    /** 회원 번호 */
+    @Id
+    private Long id;
 
+    /** 회원 탈퇴 사유 코드 */
+    private String quitReasonCode;
+
+    /** 회원 탈퇴 내용 */
+    private String quitContent;
+
+    /** 재가입 가능 일자 */
+    private LocalDateTime rejoinDt;
+
+    /** 개인정보 파기 일자 */
+    private LocalDateTime deleteDt;
 }
