6932949f519e63838a7034a28c1ee1b550fec249
JongSeong Yang <yangjs0523@naver.com>
2023-05-25 20:36:55 +0900

    feat: AuthRequest 추가

---
+++ src/main/java/com/someverything/news/dto/AuthDto.java
@@ -0,0 +1,20 @@
+package com.someverything.news.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class AuthDto {
+
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class AuthRequest {
+
+    }
+
+
+
+}
