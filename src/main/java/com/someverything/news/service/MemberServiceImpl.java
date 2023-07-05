b9c5d8bbe542bdfd522628986abb2ca08064d496
GMX\ZZ01WI766 <ksmhero@naver.com>
2023-07-05 20:05:04 +0900

    feat: MemberServiceImpl 회원가입 로직, MemberDto.MemberRequest 멤버 변수 추가

--- src/main/java/com/someverything/news/service/MemberServiceImpl.java
+++ src/main/java/com/someverything/news/service/MemberServiceImpl.java
@@ -1,12 +1,18 @@
 package com.someverything.news.service;
 
 import com.someverything.news.config.AppConfig;
+import com.someverything.news.domain.AuthCode;
 import com.someverything.news.domain.Member;
+import com.someverything.news.domain.MemberDetail;
 import com.someverything.news.dto.MemberDto;
+import com.someverything.news.global.utils.JwtTokenProvider;
+import com.someverything.news.repository.AuthCodeRepository;
+import com.someverything.news.repository.MemberDetailRepository;
 import com.someverything.news.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,13 +21,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberDetailRepository memberDetailRepository;
     private final AppConfig appConfig;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public Member createMember(MemberDto.MemberRequest request){
         Member member = appConfig.strictModelMapper().map(request, Member.class);
-        return memberRepository.save(member);
+
+        // PasswordEncoder 넣기
+
+        memberRepository.save(member);
+
+        MemberDetail memberDetail = appConfig.strictModelMapper().map(request, MemberDetail.class);
+        memberDetail.setMember(member); // MemberDetail 엔티티 관계 설정
+        memberDetailRepository.save(memberDetail);
+
+        return member;
     }
 
     @Override
